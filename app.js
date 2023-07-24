ce9358249a1a3f664bc6156656f131ec591e70e1
Belal <mohammadvns121@gmail.com>
2023-07-24 17:43:32 +0530

    Creating the Users table

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -20,13 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('64be65474f4099bc0e3e8142')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -35,5 +36,6 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
+  
   app.listen(3000);
 });
--- models/user.js
+++ models/user.js
@@ -1,16 +1,39 @@
-const Sequelize = require('sequelize');
+mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+const ObjectId = mongodb.ObjectId;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    return db.collection('users')
+      .insertOne(this)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+   return db.collection('users')
+   .findOne({_id: new ObjectId(userId)})
+   .then(user =>{
+    console.log(user)
+    return user;
+   })
+   .catch(err => {
+    console.log(err);
+  });
+  }
+}
+
+
 
 module.exports = User;
