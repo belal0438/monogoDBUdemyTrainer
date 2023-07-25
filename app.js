225d6331ba6869b1ab22b6c9049cd2647cac893c
Belal <mohammadvns121@gmail.com>
2023-07-25 17:27:46 +0530

    lets start with mongoose

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+
 
 const User = require('./models/user');
 
@@ -22,12 +23,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('64be65474f4099bc0e3e8142')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+  // User.findById('64be65474f4099bc0e3e8142')
+  //   .then(user => {
+  //     req.user = new User(user.name, user.email, user.cart, user._id);
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next()
 });
 
 app.use('/admin', adminRoutes);
@@ -35,7 +37,8 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  
-  app.listen(3000);
+mongoose.connect('mongodb+srv://mohdbelal5266:VcPdOqqLUgQ8bxZp@cluster0.nz9atgd.mongodb.net/shop?retryWrites=true&w=majority').then(result =>{
+app.listen(3000)
+}).catch(err =>{
+  console.log(err)
 });
